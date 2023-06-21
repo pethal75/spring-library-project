efc406886f8d7d89015ee73d67c5c0b72e9db1e2
peter <Peter.Halada@bol-systemhaus.de>
2023-06-21 17:49:07 +0200

    Tests

--- src/test/java/sk/library/AuthorRepositoryTests.java
+++ src/test/java/sk/library/AuthorRepositoryTests.java
@@ -2,9 +2,11 @@ package sk.library;
 
 import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -69,5 +71,17 @@ class AuthorRepositoryTests {
 		List<AuthorSurname> authorList = repository.findAllByOrderByName();
 
 		assertThat(authorList).hasSize(10);
+	}
+
+	@Test
+	void testDataIntegrityException() {
+
+		List<Author> authorList = repository.findAll();
+
+		Author author = new Author();
+		author.setName(authorList.get(0).getName());
+		author.setSurname(authorList.get(0).getSurname());
+
+		assertThrows(DataIntegrityViolationException.class, () -> repository.save(author));
 	}
 }
