5917530cea0705e662d10452ad986671e93b1c76
peter <Peter.Halada@bol-systemhaus.de>
2023-07-19 21:02:19 +0200

    Fixed tests

--- src/main/java/sk/library/controller/LibraryController.java
+++ src/main/java/sk/library/controller/LibraryController.java
@@ -19,14 +19,37 @@ import sk.library.model.Library;
 import sk.library.repository.LibraryRepository;
 
 @RestController
-@RequestMapping(value = "/api/libraries", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(value = "/api/v1/libraries", produces = MediaType.APPLICATION_JSON_VALUE)
 public class LibraryController {
 
     @Autowired
     LibraryRepository libraryRepository;
 
+
+
+    @GetMapping(value = "/test/{id}")
+    public ResponseEntity<String> test(@PathVariable("id") Long id,
+                             @RequestParam(value = "filter", required = false) String filter,
+                             @RequestBody(required = false) String body)  {
+
+        if (filter == null)
+            return ResponseEntity.badRequest().body("Chybne data");
+
+        if (body == null)
+            return ResponseEntity.badRequest().body("Chyba request body");
+
+        return ResponseEntity.ok("OK");
+    }
+
+
+
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<Library> getLibrary(@PathVariable("id") Long id) {
+
+        if (id == null || id < 0) {
+            return ResponseEntity.badRequest().build();
+        }
 
         Optional<Library> libraryOptional = libraryRepository.findById(id);
 
@@ -46,19 +69,12 @@ public class LibraryController {
     }
 
     @GetMapping(value = "/search")
-    public ResponseEntity<List<Library>> searchLibraries(@RequestParam(name = "nameQuery", required = false) String name) {
+    public ResponseEntity<List<Library>> searchLibraries(
+            @RequestParam(name = "nameQuery", required = false) String name) {
 
         List<Library> libraries = libraryRepository.findByNameContaining(name);
 
         return ResponseEntity.ok(libraries);
-    }
-
-    @GetMapping(value = "/test")
-    public Library testEndpoint() {
-
-        Library library = new Library();
-        library.setName("TEST");
-        return library;
     }
 
     @GetMapping(value = "/testException500")
