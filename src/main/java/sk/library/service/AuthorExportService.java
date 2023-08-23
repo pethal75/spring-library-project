1da1824653c47ba3d17a263549275e9e63e9798e
peter <Peter.Halada@bol-systemhaus.de>
2023-08-23 18:55:56 +0200

    Fixing unit tests

--- src/main/java/sk/library/service/AuthorExportService.java
+++ src/main/java/sk/library/service/AuthorExportService.java
@@ -1,7 +1,34 @@
 package sk.library.service;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import sk.library.dto.AuthorRecord;
+import sk.library.dto.AuthorsExport;
+import sk.library.model.Author;
+import sk.library.repository.AuthorRepository;
 
 @Service
 public class AuthorExportService {
+
+    @Autowired
+    AuthorRepository authorRepository;
+
+    public AuthorsExport exportAllAuthors() {
+
+        AuthorsExport authorsExport = new AuthorsExport();
+
+        List<Author> authors = authorRepository.findAll();
+
+        for(Author author : authors) {
+            AuthorRecord record = AuthorRecord.builder()
+                    .name(author.getName() + " " + author.getSurname())
+                    .bookCount((long) author.getBooks().size())
+                    .build();
+
+            authorsExport.getListAuthors().add(record);
+        }
+
+        return authorsExport;
+    }
 }
