d6e8e03001afad0f7ca1ffbd8d43617f37655e98
peter <Peter.Halada@bol-systemhaus.de>
2023-09-13 20:30:34 +0200

    Logging

--- src/main/java/sk/library/service/ReminderTask.java
+++ src/main/java/sk/library/service/ReminderTask.java
@@ -1,20 +1,34 @@
 package sk.library.service;
 
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import sk.library.model.Book;
+import sk.library.repository.BookRepository;
 
 @Component
 @Slf4j
 public class ReminderTask {
 
+    @Autowired
+    BookRepository repository;
+
     @Scheduled(fixedRate = 3000)
     public void runTask() {
         log.info("Running task");
-    }
 
-    @Scheduled(fixedRate = 2000)
-    public void runTask2() {
-        log.info("Running task 2");
+
+        List<Book> books = repository.findAll();
+
+        for(Book book : books) {
+            if (book.getCount() > 0)
+                log.info("Checking book " + book.getName());
+        }
+
+        books.stream()
+            .filter(book -> book.getCount() > 0)
+            .forEach(book -> log.info("Checking book " + book.getName()));
     }
 }
